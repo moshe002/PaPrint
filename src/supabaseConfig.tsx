7e12626345bbcc95062aa4700578741642f23e6e
Moses <mosesfat@gmail.com>
2023-08-10 16:58:18 +0800

    config changes

--- src/supabaseConfig.tsx
+++ src/supabaseConfig.tsx
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseKey = import.meta.env.VITE_REACT_APP_ANON_KEY;
+const supabaseKey = import.meta.env.VITE_SUPABASE_API_KEY;
 //const supabaseUrl = import.meta.env.VITE_REACT_APP_SUPABASE_URL;
 
 const supabase = createClient('https://mzglgldsmcjedhciycas.supabase.co', supabaseKey)
