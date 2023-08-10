49f6595c2257e67f61115473e4c5731e5f3b0cc0
Moses <mosesfat@gmail.com>
2023-08-10 16:45:46 +0800

    supabase config changes

--- src/supabaseConfig.tsx
+++ src/supabaseConfig.tsx
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseKey = import.meta.env.VITE_REACT_APP_ANON_KEY;
-const supabaseUrl = import.meta.env.VITE_REACT_APP_SUPABASE_URL;
+//const supabaseUrl = import.meta.env.VITE_REACT_APP_SUPABASE_URL;
 
-const supabase = createClient(supabaseUrl, supabaseKey)
+const supabase = createClient('https://mzglgldsmcjedhciycas.supabase.co', supabaseKey)
 
 export default supabase
